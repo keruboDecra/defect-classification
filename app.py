e8a0b49dd786bba80b87bb92d428f27be50494aa
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-11-29 12:38:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,12 @@
+# app.py
+
 import streamlit as st
 import numpy as np
 from keras.models import load_model
 from keras.preprocessing import image
 from keras.applications.mobilenet_v2 import preprocess_input
 
-from tensorflow.keras.applications import MobileNetV2
 
-# Load the pre-trained MobileNetV2 model for image preprocessing
-pretrained_model = MobileNetV2(weights='imagenet', include_top=True)
-
-# Load the trained model for defect detection
-defect_model_path = 'defect_model.h5'  # Assuming your model is in the root directory
-defect_model = load_model(defect_model_path)
-
-# Define image dimensions
-img_width, img_height = 150, 150
-
-# Define the feature extraction model
-feature_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
-
-from tensorflow.keras.layers import Flatten
-
-# ...
-
-# ...
 
 def classify_image(img_path):
     # Load and preprocess the image
@@ -35,32 +18,15 @@ def classify_image(img_path):
     # Extract features using MobileNetV2
     features = feature_model.predict(img_array)
 
-    # Flatten the spatial dimensions
-    features_flat = features.reshape((1, -1))  # Reshape to (1, 32000)
+    # Predict defect using the trained model
+    prediction = defect_model.predict(features)
 
-    # Print information for debugging
-    print("Shape of features:", features_flat.shape)
-    
-    # Initialize prediction variable
-    prediction = None
-
-    try:
-        # Predict defect using the trained model
-        prediction = defect_model.predict(features_flat)
-        print("Prediction shape:", prediction.shape)
-    except Exception as e:
-        print("Error during prediction:", str(e))
-
-    # Ensure the function returns a value
-    if prediction is not None:
-        return prediction[0][0]
-    else:
-        return None
+    return prediction[0][0]
 
 # Streamlit UI
 st.title("Defect Detection Dashboard")
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
 
 if uploaded_file is not None:
     st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
@@ -70,14 +36,8 @@ if uploaded_file is not None:
     # Classify the uploaded image
     prediction = classify_image(uploaded_file)
 
-    # Display the result and additional information for debugging
-    st.write("Prediction Value:", prediction)
-
-    if prediction is not None:
-        if prediction > 0.5:
-            st.write("Defect Detected!")
-        else:
-            st.write("No Defect Detected.")
+    # Display the result
+    if prediction > 0.5:
+        st.write("Defect Detected!")
     else:
-        st.write("Error during classification. Please check the logs for details.")
-
+        st.write("No Defect Detected.")
